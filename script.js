eba9165009004fd1b76d6c30a0557ad6f54985e8
Astralapse <vladlubcenko8@gmail.com>
2024-07-08 23:50:01 +0300

    fixx

--- script.js
+++ script.js
@@ -1,50 +1,38 @@
 const navLi = document.querySelector(".navigation").querySelectorAll("li");
-const githubReposItems = document.querySelector(".githubReposItems");
 
-new fullpage("#fullpage", {
-  //options here
-  autoScrolling: true,
-  scrollHorizontally: true,
-  verticalCentered: false,
-  paddingTop: "50px",
+const sections = document.querySelectorAll(".section");
 
-  afterLoad: function (origin, destination, direction, trigger) {
-    for (let i = 0; i < navLi.length; i++) {
-      if (i === fullpage_api.getActiveSection().index()) {
-        navLi[i].classList.add("selected");
-      } else {
-        navLi[i].classList.remove("selected");
-      }
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      navLi.forEach((li) => {
+        li.classList.remove("active");
+      });
+      navLi[Number(entry.target.id) - 1].classList.add("active");
     }
-  },
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
 
-fetch(
-  "https://api.github.com/users/muddyC0de/repos?q=stars%3A%3E0&sort=stars&order=desc"
-)
-  .then((res) => res.json())
-  .then((repos) => {
-    let reposItems = "";
-    for (let rep of repos.slice(0, 3)) {
-      const date = new Date(rep.updated_at);
-      console.log(rep);
-      reposItems += `<a href=${rep.svn_url}><div class="githubReposItem">
-    <h2>${rep.name}</h2>
-    <p>${rep.description ? rep.description : ""}</p>
-    <div class='repoInfo'>
-      <span ><div class=${rep.language}></div>${rep.language}</span>
-      <span> <img src="./img/star.svg" alt=""> ${rep.stargazers_count}</span>
-      <span> <img src="./img/fork.svg" alt="">${rep.forks_count}</span>
-      <span>Updated on ${date.getDate()} ${date.toLocaleString("en-En", {
-        month: "long",
-      })}</span>
-    </div>
-  </div></a>`;
-    }
-
-    githubReposItems.insertAdjacentHTML("beforeend", reposItems);
-  });
-
 const moveToFunc = (index) => {
-  fullpage_api.moveTo(index);
+  navLi[index - 1].classList.add("active");
+  document.getElementById(index).scrollIntoView({
+    behavior: "smooth",
+  });
 };
+
+AOS.init({
+  once: false,
+  threshold: 1,
+});
